84a74af59afba3d6714bc31ecf193b262506f5c6
KartiK-R-Soni <101967649+KartiK-R-Soni@users.noreply.github.com>
2022-12-01 13:08:30 +0530

    Add files via upload

---
+++ Indicators.py
@@ -0,0 +1,75 @@
+import yfinance as yf
+# Download historical data for required stocks
+tickers = ["GOOG","ZOMATO.NS","PNB.NS"]
+ohlcv_data = {}
+
+# looping over tickers and storing OHLCV dataframe in dictionary
+for ticker in tickers:
+    temp = yf.download(ticker,period='1mo',interval='15m')
+    temp.dropna(how="any",inplace=True)
+    ohlcv_data[ticker] = temp
+
+def MACD(DF, a=12 ,b=26, c=9):
+    """function to calculate MACD
+       typical values a(fast moving average) = 12; 
+                      b(slow moving average) =26; 
+                      c(signal line ma window) =9"""
+    df = DF.copy()
+    df["ma_fast"] = df["Adj Close"].ewm(span=a, min_periods=a).mean()
+    df["ma_slow"] = df["Adj Close"].ewm(span=b, min_periods=b).mean()
+    df["macd"] = df["ma_fast"] - df["ma_slow"]
+    df["signal"] = df["macd"].ewm(span=c, min_periods=c).mean()
+    return df.loc[:,["macd","signal"]]
+
+def ATR(DF, n=14):
+    "function to calculate True Range and Average True Range"
+    df = DF.copy()
+    df["H-L"] = df["High"] - df["Low"]
+    df["H-PC"] = abs(df["High"] - df["Adj Close"].shift(1))
+    df["L-PC"] = abs(df["Low"] - df["Adj Close"].shift(1))
+    df["TR"] = df[["H-L","H-PC","L-PC"]].max(axis=1, skipna=False)
+    df["ATR"] = df["TR"].ewm(com=n, min_periods=n).mean()
+    return df["ATR"]    
+
+def Boll_Band(DF, n=14):
+    "function to calculate Bollinger Band"
+    df = DF.copy()
+    df["MB"] = df["Adj Close"].rolling(n).mean()
+    df["UB"] = df["MB"] + 2*df["Adj Close"].rolling(n).std(ddof=0)
+    df["LB"] = df["MB"] - 2*df["Adj Close"].rolling(n).std(ddof=0)
+    df["BB_Width"] = df["UB"] - df["LB"]
+    return df[["MB","UB","LB","BB_Width"]]
+
+def ATR(DF, n=14):
+    "function to calculate True Range and Average True Range"
+    df = DF.copy()
+    df["H-L"] = df["High"] - df["Low"]
+    df["H-PC"] = abs(df["High"] - df["Adj Close"].shift(1))
+    df["L-PC"] = abs(df["Low"] - df["Adj Close"].shift(1))
+    df["TR"] = df[["H-L","H-PC","L-PC"]].max(axis=1, skipna=False)
+    df["ATR"] = df["TR"].ewm(com=n, min_periods=n).mean()
+    return df["ATR"]
+
+def ADX(DF, n=20):
+    "function to calculate ADX"
+    df = DF.copy()
+    df["ATR"] = ATR(DF, n)
+    df["upmove"] = df["High"] - df["High"].shift(1)
+    df["downmove"] = df["Low"].shift(1) - df["Low"]
+    df["+dm"] = np.where((df["upmove"]>df["downmove"]) & (df["upmove"] >0), df["upmove"], 0)
+    df["-dm"] = np.where((df["downmove"]>df["upmove"]) & (df["downmove"] >0), df["downmove"], 0)
+    df["+di"] = 100 * (df["+dm"]/df["ATR"]).ewm(alpha=1/n, min_periods=n).mean()
+    df["-di"] = 100 * (df["-dm"]/df["ATR"]).ewm(alpha=1/n, min_periods=n).mean()
+    df["ADX"] = 100* abs((df["+di"] - df["-di"])/(df["+di"] + df["-di"])).ewm(alpha=1/n, min_periods=n).mean()
+    return df["ADX"]
+
+def renko_DF(DF, hourly_df):
+    "function to convert ohlc data into renko bricks"
+    df = DF.copy()
+    df.reset_index(inplace=True)
+    df.drop("Close",axis=1,inplace=True)
+    df.columns = ["date","open","high","low","close","volume"]
+    df2 = Renko(df)
+    df2.brick_size = 3*round(ATR(hourly_df,120).iloc[-1],0)
+    renko_df = df2.get_ohlc_data() #if using older version of the library please use get_bricks() instead
+    return renko_df
