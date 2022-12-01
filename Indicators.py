ab426e21fcdb5d9440780f59b0226d1529dfc1de
KartiK-R-Soni <101967649+KartiK-R-Soni@users.noreply.github.com>
2022-12-01 13:15:24 +0530

    Add files via upload

--- Indicators.py
+++ Indicators.py
@@ -73,3 +73,47 @@ def renko_DF(DF, hourly_df):
     df2.brick_size = 3*round(ATR(hourly_df,120).iloc[-1],0)
     renko_df = df2.get_ohlc_data() #if using older version of the library please use get_bricks() instead
     return renko_df
+
+
+def CAGR(DF):
+    "function to calculate the Cumulative Annual Growth Rate of a trading strategy"
+    df = DF.copy()
+    df["return"] = DF["Adj Close"].pct_change()
+    df["cum_return"] = (1 + df["return"]).cumprod()
+    n = len(df)/252
+    CAGR = (df["cum_return"][-1])**(1/n) - 1
+    return CAGR
+
+def volatility(DF):
+    "function to calculate annualized volatility of a trading strategy"
+    df = DF.copy()
+    df["return"] = df["Adj Close"].pct_change()
+    vol = df["return"].std() * np.sqrt(252)
+    return vol
+
+def sharpe(DF, rf):
+    "function to calculate Sharpe Ratio of a trading strategy"
+    df = DF.copy()
+    return (CAGR(df) - rf)/volatility(df)
+
+def sortino(DF, rf):
+    "function to calculate Sortino Ratio of a trading strategy"
+    df = DF.copy()
+    df["return"] = df["Adj Close"].pct_change()
+    neg_return = np.where(df["return"]>0,0,df["return"])
+    neg_vol = pd.Series(neg_return[neg_return!=0]).std() * np.sqrt(252)
+    return (CAGR(df) - rf)/neg_vol
+
+def max_dd(DF):
+    "function to calculate max drawdown"
+    df = DF.copy()
+    df["return"] = df["Adj Close"].pct_change()
+    df["cum_return"] = (1+df["return"]).cumprod()
+    df["cum_roll_max"] = df["cum_return"].cummax()
+    df["drawdown"] = df["cum_roll_max"] - df["cum_return"]
+    return (df["drawdown"]/df["cum_roll_max"]).max()
+    
+def calmar(DF):
+    "function to calculate calmar ratio"
+    df = DF.copy()
+    return CAGR(df)/max_dd(df)
